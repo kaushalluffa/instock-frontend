e3b7ad021af806b3407f2bed342ae22576c463b3
kaushalluffa <webdevelopment852@gmail.com>
2022-10-10 19:59:53 -0400

    A22AB1-7 shows warehouse inventory items using dummydatajs file

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ArrowBack from "../../assets/icons/arrow_back-24px.svg";
-import Edit from "../../assets/icons/edit-24px.svg";
 import { ReactComponent as TagsArrows } from "../../assets/icons/sort-24px.svg";
 import { ReactComponent as ChevronRight } from "../../assets/icons/chevron_right-24px.svg";
 import { ReactComponent as DeleteBtn } from "../../assets/icons/delete_outline-24px.svg";
 import { ReactComponent as EditBtn } from "../../assets/icons/edit-24px.svg";
 import "./WarehousDetails.scss";
-function WarehouseDetails({ name }) {
+//ignore this import its just dummy data everything will be replaced with a fetch request from api
+import demoData from "../../assets/data/inventories.json";
+import { Link, useParams } from "react-router-dom";
+// the following function filters out the warehouse and its inventory but it will be replaced by a fetch request
+
+////////////////////////////
+function details(wareHouseName) {
+  const filteredWarehouse = demoData.filter(
+    (d) => d.warehouseName.toLowerCase() === wareHouseName.toLowerCase()
+  );
+  return filteredWarehouse;
+}
+/////////////////////////
+function WarehouseDetails() {
+  const { location } = useParams();
+  const wareHouse = details(location);
   return (
     <div className="warehouseDetails">
       <div className="warehouseDetails__header">
@@ -14,16 +28,16 @@ function WarehouseDetails({ name }) {
           <span className="warehouseDetails__name--icon">
             <img src={ArrowBack} alt="icon" />
           </span>
-          {/* <span>{name}</span> */}
-          <h1>Name</h1>
+          <h1>{wareHouse[0]?.warehouseName}</h1>
         </div>
         <div className="warehouseDetails__button">
-          <span className="warehouseDetails__button--icon">
-            <img src={Edit} alt="icon" />
-          </span>
+          <EditBtn />
           <span>Edit</span>
         </div>
       </div>
+      {/*
+      hardcoded info inside following section will be replaced with variables when fetched from the api
+       */}
       <div className="warehouseDetails__info">
         <div className="warehouseDetails__info--address">
           <p className="label">WAREHOUSE ADDRESS:</p>
@@ -42,6 +56,9 @@ function WarehouseDetails({ name }) {
           </div>
         </div>
       </div>
+      {/*
+      hardcoded info inside above section will be replaced with variables when fetched from the api
+       */}
       <div className="warehouseDetails__inventory">
         <div className="warehouseDetails__inventory--tags">
           <div className="tag">
@@ -61,26 +78,29 @@ function WarehouseDetails({ name }) {
           </div>
         </div>
         <div className="warehouseDetails__inventory--items">
-          <div className="warehouseDetails__inventory--item">
-            <div className="itemName">
-              Television <ChevronRight />
+          {wareHouse.map((singleWareHouse) => (
+            <div
+              className="warehouseDetails__inventory--item"
+              key={singleWareHouse.id}
+            >
+              <Link to={`/inventory/${singleWareHouse.itemName.toLowerCase()}`}>
+                <div className="itemName">
+                  {singleWareHouse.itemName} <ChevronRight />
+                </div>
+              </Link>
+              <div className="itemCategory">{singleWareHouse.category}</div>
+              <div className={singleWareHouse.status === 'In Stock'? 'instock itemStatus': 'outofstock itemStatus'}>{singleWareHouse.status}</div>
+              <div className="itemQty">{singleWareHouse.quantity}</div>
+              <div className="itemActions">
+                <div className="deleteBtn">
+                  <DeleteBtn />
+                </div>
+                <div className="editBtn">
+                  <EditBtn />
+                </div>
+              </div>
             </div>
-            <div className="itemCategory">
-              Electronics
-            </div>
-            <div className="itemStatus">
-              IN STOCK
-            </div>
-            <div className="itemQty">
-              500
-            </div>
-            <div className="itemActions">
-              <div className="deleteBtn"><DeleteBtn/></div>
-              <div className="editBtn"><EditBtn/></div>
-
-            </div>
-
-          </div>
+          ))}
         </div>
       </div>
     </div>
