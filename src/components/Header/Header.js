e8cd2ea1b4886441cac7f73c2095735a28fd6a12
falgunprajapati <falgunprajapati8@gmail.com>
2022-10-10 17:10:50 -0400

    establised basic skelaton for the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,26 @@
 import "./header.scss"
 import React from "react"
+import { Link } from "react-router-dom"
+import Logo from "../../assets/logo/InStock-Logo_1x.png"
 
 export default function Header() {
-  return <div className="header">Header</div>
+  return (
+    <div className="header">
+      <div className="header-wrapper">
+        <Link to="/">
+          <img src={Logo} className="navbar__logo"></img>
+        </Link>
+        <div className="navbar">
+          <div className="navbar__link">
+            <Link to="/warehouse">
+              <button className="navbar__link--btn btn">Warehouse</button>
+            </Link>
+            <Link to="/inventory">
+              <button className="navbar__link--btn btn">Inventory</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
