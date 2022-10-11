4fb5b754a869f5322a1dfc67b78f6abb5d314134
falgunprajapati <falgunprajapati8@gmail.com>
2022-10-11 16:08:11 -0400

    final commit for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,12 @@ import { Link } from "react-router-dom"
 import Logo from "../../assets/logo/InStock-Logo.svg"
 
 export default function Header() {
+  const { useState, Fragment } = React
+  const [active, setActive] = useState("")
+  const handleClick = (event) => {
+    setActive(event.target.id)
+  }
+
   return (
     <div className="header">
       <div className="header-wrapper">
@@ -13,12 +19,28 @@ export default function Header() {
         <div className="navbar">
           <div className="navbar__link">
             <Link to="/warehouse">
-              <button className="navbar__link--btn active btn">
+              <button
+                key={1}
+                className={
+                  active === "1" ? "active navbar__link--btn" : "inActive"
+                }
+                id={"1"}
+                onClick={handleClick}
+              >
                 Warehouse
               </button>
             </Link>
             <Link to="/inventory">
-              <button className="navbar__link--btn btn">Inventory</button>
+              <button
+                key={2}
+                className={
+                  active === "2" ? "active navbar__link--btn" : "inActive"
+                }
+                id={"2"}
+                onClick={handleClick}
+              >
+                Inventory
+              </button>
             </Link>
           </div>
         </div>
