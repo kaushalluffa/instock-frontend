b27379c7da5b234930896c3e8a08568115752cfe
falgunprajapati <falgunprajapati8@gmail.com>
2022-10-10 22:44:32 -0400

    merging develop

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,26 +1,26 @@
-import React from "react";
-import ArrowBack from "../../assets/icons/arrow_back-24px.svg";
-import { ReactComponent as TagsArrows } from "../../assets/icons/sort-24px.svg";
-import { ReactComponent as ChevronRight } from "../../assets/icons/chevron_right-24px.svg";
-import { ReactComponent as DeleteBtn } from "../../assets/icons/delete_outline-24px.svg";
-import { ReactComponent as EditBtn } from "../../assets/icons/edit-24px.svg";
-import "./WarehousDetails.scss";
+import React from "react"
+import ArrowBack from "../../assets/icons/arrow_back-24px.svg"
+import { ReactComponent as TagsArrows } from "../../assets/icons/sort-24px.svg"
+import { ReactComponent as ChevronRight } from "../../assets/icons/chevron_right-24px.svg"
+import { ReactComponent as DeleteBtn } from "../../assets/icons/delete_outline-24px.svg"
+import { ReactComponent as EditBtn } from "../../assets/icons/edit-24px.svg"
+import "./WarehousDetails.scss"
 //ignore this import its just dummy data everything will be replaced with a fetch request from api
-import demoData from "../../assets/data/inventories.json";
-import { Link, useParams } from "react-router-dom";
+import demoData from "../../assets/data/inventories.json"
+import { Link, useParams } from "react-router-dom"
 // the following function filters out the warehouse and its inventory but it will be replaced by a fetch request
 
 ////////////////////////////
 function details(wareHouseName) {
   const filteredWarehouse = demoData.filter(
     (d) => d.warehouseName.toLowerCase() === wareHouseName.toLowerCase()
-  );
-  return filteredWarehouse;
+  )
+  return filteredWarehouse
 }
 /////////////////////////
 function WarehouseDetails() {
-  const { location } = useParams();
-  const wareHouse = details(location);
+  const { location } = useParams()
+  const wareHouse = details(location)
   return (
     <div className="warehouseDetails">
       <div className="warehouseDetails__header">
@@ -28,7 +28,9 @@ function WarehouseDetails() {
           <span className="warehouseDetails__name--icon">
             <img src={ArrowBack} alt="icon" />
           </span>
-          <h1 className="warehouseDetails__name--heading">{wareHouse[0]?.warehouseName}</h1>
+          <h1 className="warehouseDetails__name--heading">
+            {wareHouse[0]?.warehouseName}
+          </h1>
         </div>
         <div className="warehouseDetails__button">
           <EditBtn />
@@ -89,7 +91,15 @@ function WarehouseDetails() {
                 </div>
               </Link>
               <div className="itemCategory">{singleWareHouse.category}</div>
-              <div className={singleWareHouse.status === 'In Stock'? 'instock itemStatus': 'outofstock itemStatus'}>{singleWareHouse.status}</div>
+              <div
+                className={
+                  singleWareHouse.status === "In Stock"
+                    ? "instock itemStatus"
+                    : "outofstock itemStatus"
+                }
+              >
+                {singleWareHouse.status}
+              </div>
               <div className="itemQty">{singleWareHouse.quantity}</div>
               <div className="itemActions">
                 <div className="deleteBtn">
@@ -104,7 +114,7 @@ function WarehouseDetails() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default WarehouseDetails;
+export default WarehouseDetails
