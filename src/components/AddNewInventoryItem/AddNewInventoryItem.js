96a695783faa93b11fa1b045c5f110fa482e7c24
kaushalluffa <webdevelopment852@gmail.com>
2022-10-13 19:45:51 -0400

    A22AB1-20 addnewitem component's header completed

--- src/components/AddNewInventoryItem/AddNewInventoryItem.js
+++ src/components/AddNewInventoryItem/AddNewInventoryItem.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import {ReactComponent as ArrowBack} from '../../assets/icons/arrow_back-24px.svg'
 import './AddNewInventoryItem.scss'
 
 const AddNewInventoryItem = () => {
   return (
     <div className="addNewItem">
-        
+      <div className="addNewItem__header">
+        <div className="warehouseDetails__name">
+          <span className="warehouseDetails__name--icon">
+            <ArrowBack/>
+          </span>
+          <h1 className="warehouseDetails__name--heading">
+            Add New Inventory Item
+          </h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AddNewInventoryItem
