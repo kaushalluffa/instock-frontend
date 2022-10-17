afb8b80e1e5bc47f6c64e5a644ae765aec8494da
Victor Arabi <victorarabi@hotmail.com>
2022-10-17 16:06:28 -0400

    refactored code to be able to upper case location name for warehouses that have two words for name/code cleanup - no working DELETE function yet

--- src/components/DeleteWarehouse/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/DeleteWarehouse.js
@@ -3,13 +3,31 @@ import { useParams, useHistory } from 'react-router-dom';
 import './DeleteWarehouse.scss';
 import closeButton from '../../assets/icons/close-24px.svg';
 
-////////////////////////////
+//Function that changes the first letter of the location coming through useParams() to upper case
+function upperCaseFirstLetter(string) {
+  if (!string) {
+    return 'Invalid Location';
+  }
+  let spaceIndex = string.indexOf(' ');
+  if (spaceIndex === -1) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  } else {
+    let secondWordIndex = spaceIndex + 1;
+    return (
+      string.charAt(0).toUpperCase() +
+      string.slice(1, spaceIndex) +
+      ' ' +
+      string.charAt(secondWordIndex).toUpperCase() +
+      string.slice(secondWordIndex + 1)
+    );
+  }
+}
+
 export default function DeleteWarehouse() {
   let history = useHistory();
   let location = useParams();
   //variable that capitalizes the first
-  let warehouse =
-    location.location.charAt(0).toUpperCase() + location.location.slice(1);
+  let warehouse = upperCaseFirstLetter(location.location);
 
   //function that handles click outside of modal area and cancel button
   function back(e) {
@@ -21,7 +39,7 @@ export default function DeleteWarehouse() {
 
   //function that deletes warehouse and send user back to previous page
   function submitDelete(e) {
-    //////////////////////////will add DELETE request
+    //will add DELETE request
     //prevent multiple clicks to bring the use further back into the route
     e.stopPropagation();
     //go back to the route in which this was called
