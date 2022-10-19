4106b453f873a96337a55d883ad2d36f24cfdd21
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-18 20:34:35 -0700

    importing icons

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -1,3 +1,10 @@
+import React from "react";
+import "./InventoryList.scss";
+import searchIcon from "../../assets/icons/search-24px.svg";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px-blue.svg";
+import rightIcon from "../../ass/ic/chevron_right-24px.svg";
+
 function InventoryList() {
   return <div className="inventoryList"></div>;
 }
