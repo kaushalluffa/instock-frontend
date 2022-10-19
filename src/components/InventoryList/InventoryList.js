c595ca6f65c0da36f52549b196720e363b25715a
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-18 23:47:33 -0700

    Checking routes for inventoryList

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,9 +3,9 @@ import "./InventoryList.scss";
 import searchIcon from "../../assets/icons/search-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px-blue.svg";
-import rightIcon from "../../ass/ic/chevron_right-24px.svg";
+import rightIcon from "../../assets/icons/chevron_right-24px.svg";
 
 function InventoryList() {
-  return <div className="inventoryList"></div>;
+  return <div className="inventoryList">INVENTORY</div>;
 }
 export default InventoryList;
