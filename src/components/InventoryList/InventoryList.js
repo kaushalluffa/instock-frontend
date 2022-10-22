f5c64b6d395b3ee38478a6b35219f01faef7d70e
Caitlin Klosterman <97420891+croseklo@users.noreply.github.com>
2022-10-22 15:51:51 -0700

    sass styling for tablet and desktop

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -5,6 +5,7 @@ import searchIcon from "../../assets/icons/search-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px-blue.svg";
 import rightIcon from "../../assets/icons/chevron_right-24px.svg";
+import sortIcon from "../../assets/icons/sort-24px.svg";
 import axios from "axios";
 
 function InventoryList() {
@@ -72,40 +73,57 @@ function InventoryList() {
         ))}
       </div>
       {/* create another contaiener for tablet and desktop ---> display none for mobile */}
-      {/* <div>
+      <div className="invList__mobile--hidden">
+        <div className="invList__items--titles">
+          <section className="invList__items--sort">
+            <p className="invList__items--title">INVENTORY ITEM</p>
+            <img src={sortIcon} />
+          </section>
+          <section className="invList__items--sort">
+            <p className="invList__items--title">CATEGORY</p>
+            <img src={sortIcon} />
+          </section>
+          <section className="invList__items--sort">
+            <p className="invList__items--title">STATUS</p>
+            <img src={sortIcon} />
+          </section>
+          <section className="invList__items--sort">
+            <p className="invList__items--title">QTY</p>
+            <img src={sortIcon} />
+          </section>
+          <section className="invList__items--sort">
+            <p className="invList__items--title">WAREHOUSE</p>
+            <img src={sortIcon} />
+          </section>
+          <section className="invList__items--sort">
+            <p className="invList__items--title">ACTIONS</p>
+            <img src={sortIcon} />
+          </section>
+        </div>
         {allInventories.map((inventory) => (
-          <div className="invList_items" key={inventory.id}>
-            <div className="invList_items-info">
-              <section className="invList_items-left">
-                <p className="invList_items-title">INVENTORY ITEM</p>
-                <p className="invList_items-item">
-                  {inventory.itemName} <img src={rightIcon} />
-                </p>
-                <p className="invList_items-title">CATEGORY</p>
-                <p className="invList_items-category">{inventory.category}</p>
-              </section>
-              <section className="invList_items-right">
-                <p className="invList_items-title">STATUS</p>
-                <p className="invList_items-instock">{inventory.status}</p>
-                <p className="invList_items-title">QTY</p>
-                <p className="invList_items-qty">{inventory.quantity}</p>
-                <p className="invList_items-title">WAREHOUSE</p>
-                <p className="invList_items-warehouse">
-                  {inventory.warehouseName}
-                </p>
+          <div className="invList__items" key={inventory.id}>
+            <div className="invList__items--info">
+              <p className="invList__items--item">
+                {inventory.itemName} <img src={rightIcon} />
+              </p>
+              <p className="invList__items--category">{inventory.category}</p>
+              <p className="invList__items--instock">{inventory.status}</p>
+              <p className="invList__items--qty">{inventory.quantity}</p>
+              <p className="invList__items--warehouse">
+                {inventory.warehouseName}
+              </p>
+              <section className="invList__items--icons">
+                <button className="invList__items--button">
+                  <img src={deleteIcon} />
+                </button>
+                <button className="invList__items--button">
+                  <img src={editIcon} />
+                </button>
               </section>
             </div>
-            <section className="invList_items-icons">
-              <button className="invList_items-button">
-                <img src={deleteIcon} />
-              </button>
-              <button className="invList_items-button">
-                <img src={editIcon} />
-              </button>
-            </section>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
