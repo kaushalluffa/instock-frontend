2e4826b45fce812ed1f5f201dd0c90bcbee97e72
Victor Arabi <victorarabi@hotmail.com>
2022-10-22 16:25:37 -0400

    finished styling and full API implementation. - no styling for table/mobile

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './WarehouseList.scss';
 import searchIcon from '../../assets/icons/search-24px.svg';
-import addIcon from '../../assets/icons/add-svgrepo-com.svg';
 import arrowUpDown from '../../assets/icons/noun-arrows-up-down-1979644.svg';
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
 import editIcon from '../../assets/icons/edit-24px-blue.svg';
@@ -11,12 +10,14 @@ import axios from 'axios';
 
 export default function WarehouseList() {
   let location = useLocation();
+
   const [allWarehouses, setAllWarehouses] = useState([]);
   useEffect(() => {
     axios.get('http://localhost:8080/warehouses').then((res) => {
       setAllWarehouses(res.data);
     });
   }, []);
+
   return (
     <div className="warehouses-list">
       <div className="warehouses-list__header">
@@ -78,51 +79,70 @@ export default function WarehouseList() {
           </div>
         </div>
         <div className="warehouses-list__table-body">
-          <div className="warehouses-list__warehouse-container">
-            <div className="warehouses-list__table-wrapper">
-              <div className="warehouses-list__item-container">
-                <h4 className="warehouses-list__item-label">WAREHOUSE</h4>
-                <p className="warehouses-list__item-link">
-                  Manhattan
+          {allWarehouses.map((warehouse) => (
+            <div
+              className="warehouses-list__warehouse-container"
+              key={warehouse.id}>
+              <div className="warehouses-list__table-wrapper">
+                <div className="warehouses-list__item-container">
+                  <h4 className="warehouses-list__item-label">WAREHOUSE</h4>
+                  <Link to={`/warehouse/${warehouse?.id}`}>
+                    <p className="warehouses-list__item-link">
+                      {warehouse?.name}
+                      <img
+                        src={rightIcon}
+                        className="warehouses-list__right-icon"
+                        alt="right arrow"
+                      />
+                    </p>
+                  </Link>
+                </div>
+                <div className="warehouses-list__item-container">
+                  <h4 className="warehouses-list__item-label">ADDRESS</h4>
+                  <p className="warehouses-list__item-txt">
+                    {warehouse?.address}
+                  </p>
+                  <p className="warehouses-list__item-txt warehouses-list__item-txt--blank">
+                    DUMMY
+                  </p>
+                </div>
+                <Link
+                  to={{
+                    pathname:
+                      '/warehouse/delete/' + warehouse?.name.toLowerCase(),
+                    state: { background: location },
+                  }}>
                   <img
-                    src={rightIcon}
-                    className="warehouses-list__right-icon"
-                    alt="right arrow"
+                    src={deleteIcon}
+                    className="warehouses-list__delete-icon"
+                    alt="delete icon"
                   />
-                </p>
+                </Link>
               </div>
-              <div className="warehouses-list__item-container">
-                <h4 className="warehouses-list__item-label">ADDRESS</h4>
-                <p className="warehouses-list__item-txt">
-                  503 Broadway, New York, USA
-                </p>
+              <div className="warehouses-list__table-wrapper">
+                <div className="warehouses-list__item-container">
+                  <h4 className="warehouses-list__item-label">CONTACT NAME</h4>
+                  <p className="warehouses-list__item-txt">
+                    {warehouse?.contact?.name}
+                  </p>
+                </div>
+                <div className="warehouses-list__item-container">
+                  <h4 className="warehouses-list__item-label">
+                    CONTACT INFORMATION
+                  </h4>
+                  <p className="warehouses-list__item-txt">
+                    {warehouse?.contact?.phone}
+                    <br /> {warehouse?.contact?.email}
+                  </p>
+                </div>
+                <img
+                  src={editIcon}
+                  className="warehouses-list__edit-icon"
+                  alt="edit icon"
+                />
               </div>
-              <img
-                src={deleteIcon}
-                className="warehouses-list__delete-icon"
-                alt="delete icon"
-              />
             </div>
-            <div className="warehouses-list__table-wrapper">
-              <div className="warehouses-list__item-container">
-                <h4 className="warehouses-list__item-label">CONTACT NAME</h4>
-                <p className="warehouses-list__item-txt">Parmin Aujla</p>
-              </div>
-              <div className="warehouses-list__item-container">
-                <h4 className="warehouses-list__item-label">
-                  CONTACT INFORMATION
-                </h4>
-                <p className="warehouses-list__item-txt">
-                  +1 (629) 555-0129 paujla@instock.com
-                </p>
-              </div>
-              <img
-                src={editIcon}
-                className="warehouses-list__edit-icon"
-                alt="edit icon"
-              />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
