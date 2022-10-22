cd44b8b645cf29afc3025c0fd480675d1214e8e7
Victor Arabi <victorarabi@hotmail.com>
2022-10-22 19:18:32 -0400

    finished styling for all sizes

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './WarehouseList.scss';
 import searchIcon from '../../assets/icons/search-24px.svg';
-import arrowUpDown from '../../assets/icons/noun-arrows-up-down-1979644.svg';
+import arrowUpDown from '../../assets/icons/sort-24px.svg';
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
 import editIcon from '../../assets/icons/edit-24px-blue.svg';
 import rightIcon from '../../assets/icons/chevron_right-24px.svg';
@@ -22,60 +22,67 @@ export default function WarehouseList() {
     <div className="warehouses-list">
       <div className="warehouses-list__header">
         <h1 className="warehouses-list__title">Warehouses</h1>
-        <div className="warehouses-list__search-container">
-          <input
-            type={'text'}
-            className="warehouses-list__search-input"
-            placeholder="Search.."
-          />
-          <img
-            src={searchIcon}
-            className="warehouses-list__search-icon"
-            alt="search-icon"
-          />
-        </div>
-        <div className="warehouses-list__btn">
-          <p className="warehouses-list__btn-txt">+ Add New Warehouse</p>
+        <div className="warehouses-list__action-wrapper">
+          <div className="warehouses-list__search-container">
+            <input
+              type={'text'}
+              className="warehouses-list__search-input"
+              placeholder="Search.."
+            />
+            <img
+              src={searchIcon}
+              className="warehouses-list__search-icon"
+              alt="search-icon"
+            />
+          </div>
+          <div className="warehouses-list__btn">
+            <p className="warehouses-list__btn-txt">+ Add New Warehouse</p>
+          </div>
         </div>
       </div>
       <div className="warehouses-list__table-container">
         <div className="warehouses-list__table-header">
-          <div className="warehouses-list__table-title">
-            <span>WAREHOUSE</span>
-            <img
-              src={arrowUpDown}
-              className="warehouses-list__arrow-up-down"
-              alt="arrow"
-            />
+          <div className="warehouses-list__table-align-left">
+            <div className="warehouses-list__table-header-wrapper">
+              <p className="warehouses-list__table-title">WAREHOUSE</p>
+              <img
+                src={arrowUpDown}
+                className="warehouses-list__arrow-up-down"
+                alt="arrow"
+              />
+            </div>
+            <div className="warehouses-list__table-header-wrapper">
+              <p className="warehouses-list__table-title">ADDRESS</p>
+              <img
+                src={arrowUpDown}
+                className="warehouses-list__arrow-up-down"
+                alt="arrow"
+              />
+            </div>
+            <Link />
           </div>
-
-          <div className="warehouses-list__table-title">
-            <span>ADDRESS</span>
-            <img
-              src={arrowUpDown}
-              className="warehouses-list__arrow-up-down"
-              alt="arrow"
-            />
-          </div>
-
-          <div className="warehouses-list__table-title">
-            <span>CONTACT NAME</span>
-            <img
-              src={arrowUpDown}
-              className="warehouses-list__arrow-up-down"
-              alt="arrow"
-            />
-          </div>
-          <div className="warehouses-list__table-title">
-            <span>CONTACT INFORMATION</span>
-            <img
-              src={arrowUpDown}
-              className="warehouses-list__arrow-up-down"
-              alt="arrow"
-            />
-          </div>
-          <div className="warehouses-list__table-title warehouses-list__table-title--action">
-            <span>ACTIONS</span>
+          <div className="warehouses-list__table-align-right">
+            <div className="warehouses-list__table-header-wrapper">
+              <p className="warehouses-list__table-title">CONTACT NAME</p>
+              <img
+                src={arrowUpDown}
+                className="warehouses-list__arrow-up-down"
+                alt="arrow"
+              />
+            </div>
+            <div className="warehouses-list__table-header-wrapper">
+              <p className="warehouses-list__table-title">
+                CONTACT INFORMATION
+              </p>
+              <img
+                src={arrowUpDown}
+                className="warehouses-list__arrow-up-down"
+                alt="arrow"
+              />
+            </div>
+            <div className="warehouses-list__table-header-wrapper">
+              <p className="warehouses-list__table-title">ACTIONS</p>
+            </div>
           </div>
         </div>
         <div className="warehouses-list__table-body">
@@ -83,18 +90,20 @@ export default function WarehouseList() {
             <div
               className="warehouses-list__warehouse-container"
               key={warehouse.id}>
-              <div className="warehouses-list__table-wrapper">
+              <div className="warehouses-list__table-wrapper warehouses-list__table-wrapper--left">
                 <div className="warehouses-list__item-container">
                   <h4 className="warehouses-list__item-label">WAREHOUSE</h4>
-                  <Link to={`/warehouse/${warehouse?.id}`}>
+                  <Link
+                    className="warehouses-list__link-wrapper"
+                    to={`/warehouse/${warehouse?.id}`}>
                     <p className="warehouses-list__item-link">
                       {warehouse?.name}
-                      <img
-                        src={rightIcon}
-                        className="warehouses-list__right-icon"
-                        alt="right arrow"
-                      />
                     </p>
+                    <img
+                      src={rightIcon}
+                      className="warehouses-list__right-icon"
+                      alt="right arrow"
+                    />
                   </Link>
                 </div>
                 <div className="warehouses-list__item-container">
@@ -102,7 +111,7 @@ export default function WarehouseList() {
                   <p className="warehouses-list__item-txt">
                     {warehouse?.address}
                   </p>
-                  <p className="warehouses-list__item-txt warehouses-list__item-txt--blank">
+                  <p className="warehouses-list__item-txt warehouses-list__item-txt--blank mobile">
                     DUMMY
                   </p>
                 </div>
@@ -114,12 +123,12 @@ export default function WarehouseList() {
                   }}>
                   <img
                     src={deleteIcon}
-                    className="warehouses-list__delete-icon"
+                    className="warehouses-list__delete-icon mobile"
                     alt="delete icon"
                   />
                 </Link>
               </div>
-              <div className="warehouses-list__table-wrapper">
+              <div className="warehouses-list__table-wrapper warehouses-list__table-wrapper--right">
                 <div className="warehouses-list__item-container">
                   <h4 className="warehouses-list__item-label">CONTACT NAME</h4>
                   <p className="warehouses-list__item-txt">
@@ -137,9 +146,28 @@ export default function WarehouseList() {
                 </div>
                 <img
                   src={editIcon}
-                  className="warehouses-list__edit-icon"
+                  className="warehouses-list__edit-icon mobile"
                   alt="edit icon"
                 />
+                <div className="warehouses-list__table-action-wrapper tablet">
+                  <Link
+                    to={{
+                      pathname:
+                        '/warehouse/delete/' + warehouse?.name.toLowerCase(),
+                      state: { background: location },
+                    }}>
+                    <img
+                      src={deleteIcon}
+                      className="warehouses-list__delete-icon tablet"
+                      alt="delete icon"
+                    />
+                  </Link>
+                  <img
+                    src={editIcon}
+                    className="warehouses-list__edit-icon tablet"
+                    alt="edit icon"
+                  />
+                </div>
               </div>
             </div>
           ))}
